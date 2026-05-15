794d182b128c1c023ec27bffc611603cb42fd2a3
entremotivator <104661532+entremotivator@users.noreply.github.com>
2026-05-15 19:30:32 -0400

    Update social_with_rocki_dashboard.py

--- social_with_rocki_dashboard.py
+++ social_with_rocki_dashboard.py
@@ -9,7 +9,7 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="LIVE AUTO CONTENT AGENT DASHBORAD",
+    page_title="Social with RockiAI",
     page_icon="💗",
     layout="wide",
     initial_sidebar_state="expanded",
